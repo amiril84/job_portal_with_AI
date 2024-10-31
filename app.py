2dce17702f62f72c833a5ef6faf56243299182a8
Amiril <hi.amiril@gmail.com>
2024-10-31 09:45:47 +0700

    add deployment config giles

--- app.py
+++ app.py
@@ -334,6 +334,8 @@ def get_shortlisted_candidates():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
 
 
